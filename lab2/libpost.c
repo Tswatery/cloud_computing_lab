87749f2f6904cb518a0d0b9d78130e843b724b73
Tswatery <1601244044@qq.com>
2023-04-27 22:01:08 +0800

    pass basic&advanced test on post&get method

--- lab2/libpost.c
+++ lab2/libpost.c
@@ -38,27 +38,14 @@ void post_method(struct http_request *request, int server_socket)
             write(server_socket, request->content, strlen(request->content));
         }
     }else if(!strcmp(request->path, "/api/upload")){
-        // int reti = regcomp(&regex, "id=([0-9]+)&name=([A-Za-z0-9]+)", REG_EXTENDED);
-        // if(reti){
-        //     perror("re compile error");
-        //     exit(1);
-        // }
-        // reti = regexec(&regex, request->content, 3, matches, 0);
-        // if(reti){
-        //     //匹配失败
-        //     strcpy(request->path, "./data/error.json");
-        //     echo_back(request, server_socket, 404);
-        // }else {
-            //匹配成功
-            http_start_response(server_socket, 200);
-            http_send_header(server_socket, "Content-Type", http_get_mime_type(".json"));
-            char *content_len = malloc(10);
-            snprintf(content_len, 10, "%lu", strlen(request->content));
-            http_send_header(server_socket, "Content-Length", content_len);
-            free(content_len);
-            http_end_headers(server_socket);
-            write(server_socket, request->content, strlen(request->content));
-        //}   
+        http_start_response(server_socket, 200);
+        http_send_header(server_socket, "Content-Type", http_get_mime_type(".json"));
+        char *content_len = malloc(10);
+        snprintf(content_len, 10, "%lu", strlen(request->content));
+        http_send_header(server_socket, "Content-Length", content_len);
+        free(content_len);
+        http_end_headers(server_socket);
+        write(server_socket, request->content, strlen(request->content));  
     }else {
             //404
             strcpy(request->path, "./static/404.html");
